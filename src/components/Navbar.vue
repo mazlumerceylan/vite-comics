c634fa723d10c76359ae172280f3a076aab4b2d2
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-28 07:26:09 +0200

    navbar template

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-line">
+
+  </div>
+  <nav>
+    <div class="logo">
+      <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <ul class="menu">
+      <li
+        v-for="(section, index) in sections"
+        :key="index"
+        @mouseenter="activateItem(index)"
+        @mouseleave="deactivateItem(index)"
+        :class="{ active: activeItem === index }"
+      >
+        {{ section.title }}
+        <div class="hover-line"></div>
+      </li>
+    </ul>
+  </nav>
+</template>
