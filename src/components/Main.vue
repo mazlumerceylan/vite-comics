48d26bfd160a838bc9a0b5072a8e74d9f36d77e9
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-04-28 07:29:11 +0200

    main template

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <div class="content">
+      Content goes here
+    </div>
+    <div class="blue-bar">
+      <div class="card-container">
+        <div
+          v-for="(card, index) in cards"
+          :key="index"
+          class="card"
+        >
+          <img :src="card.image" alt="Immagine" />
+          <p class="text">{{ card.text }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
