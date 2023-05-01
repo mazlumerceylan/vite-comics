02620fec07a132f52ee8f664fc6c54d64f473579
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-02 01:03:59 +0200

    last fix

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/Navbar.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <Navbar />
+    <Main />
+    <Footer />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+import Navbar from "./components/Navbar.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Main,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/header.scss';
+@import './assets/scss/main.scss';
+@import './assets/scss/footer.scss';
+
+body {
+  font-family: 'Roboto', sans-serif;
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="footer-top">
       <div class="left-container">
         <ul v-for="(list, index) in lists" :key="index">
-          <li>{{ list.title }}</li>
+          <li class="title">{{ list.title }}</li>
           <li v-for="(item, itemIndex) in list.items" :key="itemIndex">
             {{ item }}
           </li>
@@ -79,95 +79,6 @@ export default {
 }
 </script>
 
-<style scoped>
-footer {
-  font-family: Arial, sans-serif;
-}
+<style lang="scss" scoped>
 
-.footer-top {
-  position: relative;
-  background-image: url('src/assets/img/footer-bg.jpg');
-  background-size: cover;
-  display: flex;
-  justify-content: space-between;
-  padding: 20px;
-  color: #fff;
-}
-
-.right-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.right-container img {
-  width: 500px;
-  z-index: 999;
-  max-width: 100%;
-  height: auto;
-  
-}
-
-.left-container {
-  display: flex;
-  justify-content: start;
-  margin-left: 180px;
-  
-}
-
-ul {
-  list-style: none;
-  padding: 10px;
-  color: white;
-}
-
-li{
-  padding: 10px;
-  cursor: pointer;
-  font-size: 15px;
-
-}
-
-.footer-bottom {
-  background-color: #222;
-  color: #fff;
-  height: 100px;
-  padding: 20px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-
-.sign-in {
-  background-color: red;
-  border: 2px solid white;
-  color: white;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.image-group {
-  display: flex;
-  align-items: center;
-}
-
-.small-images {
-  display: flex;
-  justify-content: space-between;
-  margin-left: 20px;
-  cursor: pointer;
-  
-}
-
-.small-images img {
-  width: 50px;
-  height: 50px;
-  object-fit: cover;
-  margin-left: 10px;
-}
 </style>
